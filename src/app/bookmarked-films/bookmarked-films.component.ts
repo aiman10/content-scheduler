e9193260756f9c061b8e383f536fe781916c31c3
aiman10 <aiman.ouladali@student.ap.be>
2023-09-24 21:45:45 +0200

    Add import field

--- src/app/bookmarked-films/bookmarked-films.component.ts
+++ src/app/bookmarked-films/bookmarked-films.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { BookmarkService } from '../service/bookmarked.service';
 import { MoviedatabaseService } from '../service/moviedatabase.service';
 import { SelectdateService } from '../service/selectdate.service';
-import { Papa } from 'ngx-papaparse';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bookmarked-films',
@@ -13,13 +13,16 @@ import { Papa } from 'ngx-papaparse';
 })
 export class BookmarkedFilmsComponent implements OnInit {
   bookmarkedMovies: IFilm[] = [];
+  importedMovies: any[] = [];
+  showSelect = false;
+  showImportSection = false;
+  selectAll: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private movieService: MoviedatabaseService,
     private bookmarkService: BookmarkService,
-    private dateService: SelectdateService,
-    private papa: Papa
+    private dateService: SelectdateService
   ) {}
 
   //TODO Add export/import localstorage/bookmarked movies to file
@@ -35,6 +38,20 @@ export class BookmarkedFilmsComponent implements OnInit {
 
       return dateA.getMonth() - dateB.getMonth();
     });
+  }
+
+  toggleExportSection() {
+    // Hide import section when export section is toggled
+    this.showImportSection = false;
+  }
+  toggleImportSection() {
+    this.showImportSection = !this.showImportSection;
+  }
+
+  toggleSelectAll() {
+    for (const movie of this.importedMovies) {
+      movie.selected = this.selectAll;
+    }
   }
 
   toggleBookmark(movie: IFilm) {
@@ -57,25 +74,49 @@ export class BookmarkedFilmsComponent implements OnInit {
     }
   }
 
-  exportToCSV() {
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.showSelect = !this.showSelect;
+      // Read the JSON file and populate the importedMovies array
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const content = e.target?.result as string;
+        this.importedMovies = JSON.parse(content);
+      };
+      reader.readAsText(file);
+    }
+  }
+
+  addSelectedMovies() {
+    // Add selected movies from importedMovies to bookmarkedMovies
+    const selectedMovies = this.importedMovies.filter(
+      (movie) => movie.selected
+    );
+    this.bookmarkedMovies.push(...selectedMovies);
+
+    // Clear the importedMovies array
+    this.importedMovies = [];
+  }
+
+  exportToJSON() {
     const exportData = this.bookmarkedMovies.map((movie) => ({
-      Title: movie.title,
-      ReleaseYear: new Date(movie.release_date).getFullYear(),
-      PosterPath: movie.poster_path,
-      Id: movie.id,
+      title: movie.title,
+      releaseYear: new Date(movie.release_date).getFullYear(),
+      posterPath: movie.poster_path,
+      id: movie.id,
+      isBookmarked: true,
     }));
 
-    const csv = this.papa.unparse(exportData, {
-      header: true,
-    });
+    const jsonContent = JSON.stringify(exportData, null, 2);
 
-    const blob = new Blob([csv], { type: 'text/csv' });
+    const blob = new Blob([jsonContent], { type: 'application/json' });
     const url = window.URL.createObjectURL(blob);
 
     // Create a temporary anchor element to trigger the download
     const a = document.createElement('a');
     a.href = url;
-    a.download = 'bookmarked_movies.csv';
+    a.download = 'bookmarked_movies.json';
 
     // Trigger a click event to start the download
     a.click();
