7f8729193500181eee4fde0e9d2631d2316b6cf6
aiman10 <37143774+aiman10@users.noreply.github.com>
2026-06-03 22:37:23 +0200

    feat: initialise ThemeService on app startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { ThemeService } from './service/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'content-cal';
 
   constructor(
     @Inject(DOCUMENT) public document: Document,
-    public auth: AuthService
+    public auth: AuthService,
+    private theme: ThemeService
   ) {}
+
+  ngOnInit(): void {
+    this.theme.init();
+  }
 }
